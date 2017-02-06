d00d7eb224ad48c800711bfd6baf60adb38914f6
konstantin popov <v8199@yandex.ru>
2017-02-07 03:55:51 +0600

    Init mongoose and make ability to storing token params

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-var express = require('express');
 var request = require('request');
+var express = require('express');
 var vkAuth = require('vk-auth');
-var exphbs  = require('express-handlebars');
+var mongoose = require('mongoose');
+var hbs = require('express-handlebars');
+
+mongoose.connect('mongodb://localhost/vk-assistant');
+
+// Protected user login data
+var userCredentials = {
+    email: 'v8199@yandex.ru',
+    password: 'tartar777'
+};
+
+var schema = new mongoose.Schema({
+    email: {type: String},
+    user_id: {type: Number},
+    token: {type: String},
+    exp: {type: Number},
+    date: {type: Date, default: Date.now}
+}, {collection: 'tokens'});
+
+var Token = mongoose.model('Token', schema);
 
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 var config = {
@@ -16,34 +35,83 @@ var authInstance = vkAuth(config.app_id, 'messages');
 
 app.get('/', function (req, res) {
 
-    authInstance.authorize('v8199@yandex.ru', 'tartar777', function (err, tokenParams) {
+    var method = 'messages.get';
 
-        var method = 'messages.get';
+    // Trying to get token data from DB
+    fetchToken(userCredentials.email, function (err, tokenObject) {
 
-        request.post(
-            'https://api.vk.com/method/' + method,
-            {
-                form: {
-                    access_token: tokenParams.access_token
-                }
-            },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
+        tokenObject = tokenObject[0];
 
-                    var items = JSON.parse(body).response;
-
-                    items.filter(function(value) {
-                        return typeof value == 'Object';
-                    });
-
-                    res.render('home', {
-                        items: items
-                    });
-                }
-            }
-        );
+        // If token data was fetched already
+        if (tokenObject) {
+            callApi(method, tokenObject, res);
+        } else {
+            authInstance.authorize(userCredentials.email, userCredentials.password, function (err, tokenParams) {
+                tokenParams['email'] = userCredentials.email;
+                // Save to db token collection
+                saveToken(tokenParams);
+                callApi(method, tokenParams, res);
+            });
+        }
     });
 });
+
+/**
+ * Make vk api request
+ * @param method
+ * @param tokenObject
+ * @param res
+ */
+function callApi(method, tokenObject, res) {
+    request.post(
+        'https://api.vk.com/method/' + method,
+        {
+            form: {
+                access_token: tokenObject.access_token,
+                count: 5
+            }
+        },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+
+                var items = JSON.parse(body).response;
+
+                items.filter(function (value) {
+                    return typeof value == 'object';
+                });
+
+                res.render('home', {
+                    items: items
+                });
+            }
+        }
+    );
+}
+
+
+/**
+ * Fetch token from db by email
+ * @param email
+ * @param callback
+ */
+function fetchToken(email, callback) {
+    Token.collection.find({email: email}, {limit: 1}).toArray(callback);
+}
+
+
+/**
+ * Save token to db
+ * @param params
+ */
+function saveToken(params) {
+    Token.collection.insert(params, onInsert);
+
+    function onInsert(err) {
+        if (!err) {
+            console.info('Token params were successfully stored.');
+        }
+    }
+}
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
